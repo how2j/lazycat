9899d7d7464685327598933295a433c25ca14e42
X7TI <X7TI@meepo-pc>
2017-07-25 15:39:52 +0800

    test

--- build/package.bat
+++ build/package.bat
@@ -1,4 +1,4 @@
 e:
 cd e:\project\lazycat\build
 del lazycat.rar
-"C:\Program Files\WinRAR\Rar.exe"  a  -r -idq -xE:\project\lazycat\build\package.bat lazycat.rar *.* 
+"D:\software\winrar\Rar.exe"  a  -r -idq -xE:\project\lazycat\build\package.bat lazycat.rar *.* 
